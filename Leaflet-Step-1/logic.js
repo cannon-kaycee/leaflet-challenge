5729d42018909252c0c3c7a80d21c752395e4fe7
KayCan <cannon.kaycee@gmail.com>
2022-05-15 16:27:38 -0700

    second push

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -2,7 +2,8 @@ function createMap(earthquakes) {
 
     // Create the tile layer that will be the background of our map.
     var streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        noWrap: true
     });
   
   
@@ -21,7 +22,9 @@ function createMap(earthquakes) {
       center: [40.73, -74.0059],
       zoom: 12,
       layers: [streetMap, earthquakes]
-    });
+      
+    }).setView([0, 0], 1);
+    
   
     // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
     L.control.layers(baseMaps, overlayMaps, {
@@ -32,28 +35,40 @@ function createMap(earthquakes) {
   function createMarkers(response) {
   
     // Pull the "quakes" property from response.
-    var quakes = response.type.geometry.type;
-  
+    var quakes = response.features;
+
     // Initialize an array to hold quake markers.
     var quakeMarkers = [];
+
+    var icon = L.icon({
+        iconSize: quakesItem.properties.mag,
+        iconColor: "green",
+        shape: "circle"
+
+    })
+    
+    
+    // centerMarker.options.icon;
+    //     icon.options.iconSize = [quakesItem.properties.mag];
+    //     shape: "circle",
+    //     centerMarker.setIcon(icon);
   
     // Loop through the quakes array.
     for (var index = 0; index < quakes.length; index++) {
-      var quakes = quakes[index];
+      var quakesItem = quakes[index];
   
       // For each quake, create a marker, and bind a popup with the quake's name.
-      var quakeMarker = L.marker([quakes.latitude, quakes.longitude, quakes.depth])
-        .bindPopup("<h3>" + id + "<h3><h3>Capacity: " + "</h3>");
+      var quakeMarker = L.marker([quakesItem.geometry.coordinates[1], quakesItem.geometry.coordinates[0]], {icon: icon})
+        .bindPopup("<h3>" + "<h3><h3>Place: " + quakesItem.properties.place + "<h3><h3>Magnitude: " + quakesItem.properties.mag + "<h3><h3>Depth: " + quakesItem.geometry.coordinates[2] + "</h3>")};
   
       // Add the marker to the quakeMarkers array.
      quakeMarkers.push(quakeMarker);
-    }
+  }
   
     // Create a layer group that's made from the quake markers array, and pass it to the createMap function.
     createMap(L.layerGroup(quakeMarkers));
-  }
+   
   
   
   // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
   d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
-  
