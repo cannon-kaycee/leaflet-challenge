4498c10e731b6c619f108dd609288abf708e6433
KayCan <cannon.kaycee@gmail.com>
2022-05-22 07:57:20 -0700

    map

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -2,10 +2,9 @@ function createMap(earthquakes) {
 
     // Create the tile layer that will be the background of our map.
     var streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        noWrap: true
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        // noWrap: true
     });
-  
   
     // Create a baseMaps object to hold the streetmap layer.
     var baseMaps = {
@@ -25,28 +24,30 @@ function createMap(earthquakes) {
       
     }).setView([0, 0], 1);
     
-  
     // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(map);
-  
 
-    function setIcon(mag){
-        iconSize: quakesItem.properties.mag*3
-    }
-  L.geoJSON(earthquakes, {
-      pointToLayer:function(feature,latlng) {
-          return L.circleMarker(latlng)
-      },
-      style: setIcon}).addTo(map);
+    var geojsonMarkerOptions = {
+        shape: "circle",
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
 
+    L.geojson(earthquakes, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
 
 }
-  
-  
 
-  function createMarkers(response) {
+function createMarkers(response) {
   
     // Pull the "quakes" property from response.
     var quakes = response.features;
@@ -54,52 +55,29 @@ function createMap(earthquakes) {
     // Initialize an array to hold quake markers.
     var quakeMarkers = [];
 
-    // var icon = L.icon({
-    //     iconSize: quakesItem.properties.mag,
-    //     iconColor: "green",
-    //     shape: "circle"
-
-    
-    
-    
-    
-    // centerMarker.options.icon;
-    //     icon.options.iconSize = [quakesItem.properties.mag];
-    //     shape: "circle",
-    //     centerMarker.setIcon(icon);
-  
     // Loop through the quakes array.
     for (var index = 0; index < quakes.length; index++) {
       var quakesItem = quakes[index];
+
+      var icon = L.icon({
+        iconSize: quakesItem.properties.mag,
+        iconColor: "green",
+        shape: "circle"});
   
       // For each quake, create a marker, and bind a popup with the quake's name.
-      var quakeMarker = L.marker([quakesItem.geometry.coordinates[1], quakesItem.geometry.coordinates[0]])
+      var quakeMarker = L.circleMarker([quakesItem.geometry.coordinates[1], quakesItem.geometry.coordinates[0]], {icon: icon})
         .bindPopup("<h3>" + "<h3><h3>Place: " + quakesItem.properties.place + "<h3><h3>Magnitude: " + quakesItem.properties.mag + "<h3><h3>Depth: " + quakesItem.geometry.coordinates[2] + "</h3>");
     
       // Add the marker to the quakeMarkers array.
      quakeMarkers.push(quakeMarker);
     }
-  
- 
-        
-  
-//   function setMag(mag) {
-//     if (quakesItem.properties.mag < 1)
-//     if (quakesItem.properties.mag >= 1) and (quakesItem.properties.mag < 2)
-//     if (quakesItem.properties.mag >= 2) and (quakesItem.properties.mag < 3)
-//     if (quakesItem.properties.mag >= 3) and (quakesItem.properties.mag < 4)
-//     if (quakesItem.properties.mag >= 4) and (quakesItem.properties.mag < 5)
-//     if (quakesItem.properties.mag >= 5) and (quakesItem.properties.mag < 6)
-//     if (quakesItem.properties.mag >= 6) and (quakesItem.properties.mag < 7)
-// }
 
     // Create a layer group that's made from the quake markers array, and pass it to the createMap function.
     createMap(L.layerGroup(quakeMarkers));
-   
-
 }
+
   // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
-  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
 
 
 
@@ -119,3 +97,28 @@ function createMap(earthquakes) {
 //     // We set the style for each circleMarker using our styleInfo function.
 //     style: YOUR_FUNCTION_THAT_RETURNS_THE_DICTIONARY_GOES_HERE
 //   }).addTo(map);
+
+
+//     function setIcon(mag){
+//         iconSize: quakesItem.properties.mag*3
+//     }
+//   L.geoJSON(earthquakes, {
+//       pointToLayer:function(feature,latlng) {
+//           return L.circleMarker(latlng)
+//       },
+//       style: setIcon}).addTo(map);
+
+//   function setMag(mag) {
+//     if (quakesItem.properties.mag < 1)
+//     if (quakesItem.properties.mag >= 1) and (quakesItem.properties.mag < 2)
+//     if (quakesItem.properties.mag >= 2) and (quakesItem.properties.mag < 3)
+//     if (quakesItem.properties.mag >= 3) and (quakesItem.properties.mag < 4)
+//     if (quakesItem.properties.mag >= 4) and (quakesItem.properties.mag < 5)
+//     if (quakesItem.properties.mag >= 5) and (quakesItem.properties.mag < 6)
+//     if (quakesItem.properties.mag >= 6) and (quakesItem.properties.mag < 7)
+// }
+
+    // centerMarker.options.icon;
+    //     icon.options.iconSize = [quakesItem.properties.mag];
+    //     shape: "circle",
+    //     centerMarker.setIcon(icon);
